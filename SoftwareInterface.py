576b2eafc6dcd40bf75e9597cd3747346d5c44b8
Nilly92 <n.atasayar@aol.com>
2021-09-20 22:12:40 +0200

    App Form

--- SoftwareInterface.py
+++ SoftwareInterface.py
@@ -4,20 +4,45 @@ Created on Sun Sep 19 20:37:40 2021
 
 @author: las_v
 """
+
+from kivy.uix.label import Label
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+import os
+
+class GridLay(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+
+        self.add_widget(Image(source ="Logo.jpg"))
+        self.box = BoxLayout(orientation='vertical')
+        self.button1 = Button(text="Load The File")
+        self.button2 = Button(text="Chose The Data Visualization Graph Type")
+        self.save = Button(text="Close")
+        self.box.add_widget(self.button1)
+        self.box.add_widget(self.button2)
+        self.box.add_widget(self.save)
+        self.add_widget(self.box)
+
+
 
 class DataVisu(App):
     def build(self):
-        self.window = GridLayout()
-        #add widgets to window
+        self.screen_manager = ScreenManager()
+
+        self.connect_page = ConnectPage()
+        screen = Screen(name="Connect")
+        screen.add_widget(self.connect_page)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
 
 
-        return self.window
 
 if __name__ == "__main__":
-    DataVisu().run()
+    DataVisu().App()
