a7f8b02debd764a9a5822fa468def7f83eb63b4b
Nilly92 <n.atasayar@aol.com>
2021-12-26 22:46:30 +0100

    Second Page

--- DataVisu.py
+++ DataVisu.py
@@ -10,7 +10,10 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.lang import Builder
-
+from kivy.network.urlrequest import UrlRequest
+from kivy.uix.label import Label
+from kivy.core.text import Text
+from kivy.uix.accordion import Accordion
 
 Window.clearcolor = (1, 1, 1, 1)
 
@@ -26,12 +29,14 @@ class SecondWindow(Screen):
 class MyScreenManager(ScreenManager):
     pass
 
+
+py = Builder.load_file("Data_Science_Software.py")
 kv = Builder.load_file("DataVisu.kv")
 
 class DataVisu(App):
     def build(self):
 
-        return kv
+        return 
 
 
 if __name__ == "__main__":
--- Data_Science_Software.py
+++ Data_Science_Software.py
@@ -1,25 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 28 21:02:07 2021
-
-@author: las_v
-"""
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import eurostat
 
-
-
-Data = pd.read_csv('C:/Users/las_v/OneDrive/Masaüstü/Data Science Projects/BioDiversity/Data Bio/species.csv')
-
-# Find The Location of Data Set
-# Choose the column
-
-on_press: root.btn_clk()
-
-
+Data_List = eurostat.get_toc()
+df = pd.DataFrame(Data_List)
  
 
 
